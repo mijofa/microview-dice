8bb3d3d4e24af76907b36258c5438a24c6bcb91e
Mike Abrahall <mike@abrahall.id.au>
2019-07-14 18:04:43 +1000

    Lots of cleanup, didn't change the workflow of anything

--- main.ino
+++ main.ino
@@ -1,26 +1,3 @@
-/*****************************************************************
-MicroView_Analog_Clock.ino
-SFE_LSM9DS0 Library Simple Example Code
-Jim Lindblom @ SparkFun Electronics
-Original Creation Date: February 28, 2015
-https://github.com/sparkfun/MicroView/tree/master/Libraries
-
-Turn your MicroView into an analog clock!
-
-This sketch requires the Arduino time library. Get it from
-here: http://playground.arduino.cc/Code/Time
-
-
-Development environment specifics:
-	IDE: Arduino 1.6.0
-	Hardware Platform: MicroView
-
-This code is beerware. If you see me (or any other SparkFun 
-employee) at the local, and you've found our code helpful, please 
-buy us a round!
-
-Distributed as-is; no warranty is given.
-*****************************************************************/
 #include <Arduino.h>
 #include <MicroView.h>
 
@@ -33,6 +10,8 @@ const uint8_t maxW = uView.getLCDWidth();
 const uint8_t midW = maxW/2;
 const uint8_t maxH = uView.getLCDHeight();
 const uint8_t midH = maxH/2;
+// Would like to set these as const as well, but they can't be set until after choosing a font,
+// and that's done in a deeper scope where they would then be unreadable.
 uint8_t digit_width;
 uint8_t half_digit_height;
 
@@ -43,58 +22,8 @@ int roll_result = 0;
 // This is updated by the tilt-switch pin interrupt
 volatile bool tilted = false;
 
-void drawTime() {
-  static boolean firstDraw = false;
-  static unsigned long mSec = millis() + 1000;
-  static float degresshour, degressmin, degresssec,
-               hourx, houry, minx, miny, secx, secy;
 
-  float actual_degrees;
-  float whatever_uView_calls_degrees;
-
-  // First time draw requires extra line to set up XOR's:
-  // NOTE: "first"Draw is a lie, I think this is undrawing the line from the previous run.
-  if (firstDraw) {
-    uView.circleFill(midW-1, midH-1, CLOCK_SIZE, BLACK, NORM);
-    uView.circle(midW-1, midH-1, CLOCK_SIZE, WHITE, NORM);
-//    uView.line(midW, midH, 32 + secx, 24 + secy, WHITE, XOR);
-  }
-
-  if (roll_result >= 100) {  // 3 digits
-    uView.setCursor(midW - (digit_width * 1.5), midH - half_digit_height);
-  } else if (roll_result >= 10) {  // 2 digits
-    uView.setCursor(midW - digit_width, midH - half_digit_height);
-  } else {  // 1 digit
-    uView.setCursor(midW - (digit_width / 2), midH - half_digit_height);
-  }
-  uView.print(roll_result);
-
-
-
-  actual_degrees = (analogRead(A0) / 1024.0) * 360.0;
-  actual_degrees = actual_degrees - 180;
-  // So basically, I don't understand any of this math. I just adapted it from what the original example used.
-  whatever_uView_calls_degrees = (actual_degrees + 270) * (PI / 180);
-  degresssec = whatever_uView_calls_degrees;
-  //// Calculate second hand degrees:
-  //degresssec = (((second() * 360) / 60) + 270) * (PI / 180);
-
-  // Calculate x,y coordinates of second hand:
-  secx = cos(degresssec) * (CLOCK_SIZE / 1.1);
-  secy = sin(degresssec) * (CLOCK_SIZE / 1.1);
-
-  // Draw hands with the line function:
-  uView.line(midW, midH, midW+secx, midH+secy, WHITE, XOR);
-
-  // Set firstDraw flag to true, so we don't do it again.
-  // NOTE: Uh, that's a lie, it does that when it is true.
-  firstDraw = true;
-
-  // Actually draw the hands with the display() function.
-  uView.display();
-}
-
-void setup() {
+void init_microview() {
   uView.begin();    // set up the MicroView
   uView.clear(PAGE);// erase hardware memory inside the OLED
   uView.display();  // display the content in the now cleared buffer
@@ -104,34 +33,91 @@ void setup() {
   //
   digit_width = uView.getFontWidth();
   half_digit_height = uView.getFontHeight() / 2;
+}
 
-  // Draw clock face (circle outline & text):
-  drawFace();
 
-  Serial.begin(9600);
+void drawTime() {
+  static float degresssec, secx, secy;
+  static float actual_degrees;
+  static float whatever_uView_calls_degrees;
+
+  // Clear the current face
+  {
+    uView.circleFill(midW-1, midH-1, CLOCK_SIZE, BLACK, NORM);
+    uView.circle(midW-1, midH-1, CLOCK_SIZE, WHITE, NORM);
+  }
+
+  // Figure out how wide the text is, so we can offset from the centre of the display before printing
+  if (roll_result >= 100) {  // 3 digits
+    uView.setCursor(midW - (digit_width * 1.5), midH - half_digit_height);
+  } else if (roll_result >= 10) {  // 2 digits
+    uView.setCursor(midW - digit_width, midH - half_digit_height);
+  } else {  // 1 digit
+    uView.setCursor(midW - (digit_width / 2), midH - half_digit_height);
+  }
+  // Print the current roll result.
+  uView.print(roll_result);
+
+
+  // DEBUGGING
+  // Print a line to indicate what the soft-pot on A0 is set to.
+  //
+  // So basically, I don't understand any of this math. I just adapted it from what the MicroView example code used.
+  {
+    actual_degrees = (analogRead(A0) / 1024.0) * 360.0;
+    actual_degrees = actual_degrees - 180;
+    whatever_uView_calls_degrees = (actual_degrees + 270) * (PI / 180);
+    degresssec = whatever_uView_calls_degrees;
+    //// Calculate second hand degrees:
+    //degresssec = (((second() * 360) / 60) + 270) * (PI / 180);
+
+    // Calculate x,y coordinates of second hand:
+    secx = cos(degresssec) * (CLOCK_SIZE / 1.1);
+    secy = sin(degresssec) * (CLOCK_SIZE / 1.1);
+
+    // Draw hands with the line function:
+    uView.line(midW, midH, midW+secx, midH+secy, WHITE, XOR);
+  }
+
+  // Actually draw the hands with the display() function.
+  uView.display();
+}
+
+void setup() {
+  init_microview();
+
+  // The analog pins are input by default, just explicitly setting it to self-document.
+  pinMode(A0, INPUT);
+
   // Enable a pull up resistor built in to the Arduino controller.
+  // This just means we get a much more reliable result from the tilt switch.
   // FIXME: Is that built in for every pin?
   pinMode(2, INPUT_PULLUP);
 
-  // Whenever the state changes on D2, change toggled the tilted bool.
+  // Whenever the state changes on D2, toggled the tilted bool.
   // NOTE: This uses a lambda, but I don't actually understand lambda syntax in C.
   // FIXME: Just set tilted = true and let the RNG unset it when finished with that action?
   // FIXME: roll_result is not volatile, it should not be getting set directly here
   // FIXME: Should I update the random seed?
   // FIXME: Use digitalPinToInterrupt(), ref: https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
   attachInterrupt(INT0, [] () {tilted = !tilted;roll_result = random(100);}, CHANGE);
+
+  Serial.begin(9600);  // DEBUGGING
+
 }
 
 void loop() {
   drawTime();
-  uView.invert(tilted);
-  Serial.print("D2 = ");
-  Serial.print(digitalRead(2));
-  Serial.print("; tilted = ");
-  Serial.print(tilted);
-  Serial.print("; A0 = ");
-  Serial.print(analogRead(A0) / 1024.0);
-  Serial.print("; roll = ");
-  Serial.print(roll_result);
-  Serial.println();
+
+  uView.invert(tilted);  // DEBUGGING
+
+  Serial.print("D2 = ");  // DEBUGGING
+  Serial.print(digitalRead(2));  // DEBUGGING
+  Serial.print("; tilted = ");  // DEBUGGING
+  Serial.print(tilted);  // DEBUGGING
+  Serial.print("; A0 = ");  // DEBUGGING
+  Serial.print(analogRead(A0) / 1024.0);  // DEBUGGING
+  Serial.print("; roll = ");  // DEBUGGING
+  Serial.print(roll_result);  // DEBUGGING
+  Serial.println();  // DEBUGGING
 }
